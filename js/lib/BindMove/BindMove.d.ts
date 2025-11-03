f60a63758bfe5b19c347c70865b92635dd84f514
shc0743 <shc0743@outlook.com>
2025-11-04 03:50:22 +0800

    Revert "refactor: split addCSS into standalone package"
    This reverts commit 33e2bf2b79f1825a75993163e2bd4bca9d18aab2.

--- js/lib/BindMove/BindMove.d.ts
+++ js/lib/BindMove/BindMove.d.ts
@@ -48,7 +48,16 @@ declare module "bindmove" {
      * @param el 要解除绑定的元素（对应BindMove方法的`el`而不是`target`）
      */
     export function UnBindMove(el: HTMLElement): void;
-    
+
+    /**
+     * 添加CSS样式
+     * @param css CSS样式字符串
+     * @param el 要添加到的元素，默认为 null (添加到document)
+     * @param adopt 是否使用 adoptedStyleSheets，默认为 false
+     * @returns 返回添加的样式表或style元素
+     */
+    export function addCSS(css: string, el?: Document | ShadowRoot | null): CSSStyleSheet | HTMLStyleElement;
+
     /**
      * 绑定移动功能使用的CSS样式
      */
