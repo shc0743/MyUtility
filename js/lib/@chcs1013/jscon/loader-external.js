eb1c9ab874c7f64b7591a7698ac4a0dc1fd420e7
shc0743 <shc0743@outlook.com>
2025-12-17 03:10:34 +0800

    fix(jscon): fix typo

--- js/lib/@chcs1013/jscon/loader-external.js
+++ js/lib/@chcs1013/jscon/loader-external.js
@@ -1,7 +1,7 @@
-import { JsCon } from 'cdn.jsdelivr.net/npm/@chcs1013/jscon@0.1.0/jscon.js'
+import { JsCon } from 'https://cdn.jsdelivr.net/npm/@chcs1013/jscon@0.1.1/jscon.js'
 const con = new JsCon();
 export { con as default };
 con.registerConsoleAPI(globalThis.console);
 con.addErrorHandler();
 
-// usage: import("cdn.jsdelivr.net/npm/@chcs1013/jscon@0.1.0/loader-external.js")
+// usage: import("https://cdn.jsdelivr.net/npm/@chcs1013/jscon@0.1.1/loader-external.js")
